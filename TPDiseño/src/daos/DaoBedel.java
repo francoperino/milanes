da326e56a5ea7cdeb31538ce97d6e230d8a9fb5f
franc <francoperino@hotmail.com.ar>
2017-11-08 16:12:35 -0300

    Buscar por turno y apellido

--- TPDiseño/src/daos/DaoBedel.java
+++ TPDiseño/src/daos/DaoBedel.java
@@ -106,13 +106,36 @@ public class DaoBedel {
             s.getTransaction().commit();
             
             return lista;
-    }/*
-    public List <Bedel> buscarPorTurnoyApellido(String turno,String apellido){
-        Bedel bedel = new Bedel();
-        Session s = HibernateUtil.getSessionFactory().getCurrentSession();
-        s.beginTransaction();
-        List<Bedel> lista = s.createQuery("select apellido from Usuario where( )")
-    }*/
+    }
+    public List <Map> buscarPorTurnoyApellido(String turno,String apellido){
+       Session s =  HibernateUtil.getSessionFactory().getCurrentSession();
+            s.beginTransaction();
+            Query query;
+            
+            if(turno.equals("Todos")){
+                query= s.createQuery("select new map(b.turno,u.nombre,u.apellido,u.nickusuario) from Usuario u, Bedel b where    (b.nickusuario = u.nickusuario) and (upper(u.apellido) LIKE upper(:apellido)) ").setParameter("apellido", apellido+"%"); 
+            }
+            else{
+               query= s.createQuery("select new map(b.turno,u.nombre,u.apellido,u.nickusuario) from Usuario u, Bedel b where    (b.nickusuario = u.nickusuario) and (b.turno=:turno) and (upper(u.apellido) LIKE upper(:apellido))").setParameter("turno", turno).setParameter("apellido", apellido+"%"); 
+               
+            }
+            List<Map> lista = query.list();
+           
+            for(int i=0; i<lista.size();i++){
+                Map mapa = lista.get(i);
+                Set llaves = mapa.keySet();
+                for(Iterator<String> it = llaves.iterator(); it.hasNext();){
+                    String llaveActual = it.next();
+                    System.out.println( mapa.get(llaveActual));
+                }
+                 
+                
+            }
+            
+            s.getTransaction().commit();
+            
+            return lista;
+    }
     public List <Bedel> consultarNickBedel(String nickUsuario){
      
        Bedel bedel = new Bedel();
